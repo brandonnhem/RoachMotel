8afa38e75908bf0d76ddb2bc30bee2f5440599f5
brandonnhem <brandonnhem@gmail.com>
2018-12-10 11:25:25 -0800

    Added java doc comments

--- src/patterns/AmenitiesDecorator.java
+++ src/patterns/AmenitiesDecorator.java
@@ -1,5 +1,17 @@
 package patterns;
 
+/**
+ * 
+ * @author Jamil Khan
+ * @author Brandon Nhem
+ * @author Anthony Pham
+ * @author Brandon Walker
+ * Purpose: Decorator pattern for the MotelRoom objects
+ * Input: n/a
+ * Output: n/a
+ *
+ */
+
 public abstract class AmenitiesDecorator extends MotelRoom{
     public abstract String getDescription();
 }
